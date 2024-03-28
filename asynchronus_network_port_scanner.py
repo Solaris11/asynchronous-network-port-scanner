6c7af79d5ac0f9cbc2bef037cf61b6cd53cdaf94
Ethan Bayram <emrah.bayram@disney.com>
2024-03-28 01:17:46 -0700

    Updating config

--- asynchronus_network_port_scanner.py
+++ asynchronus_network_port_scanner.py
@@ -1,33 +1,38 @@
 import asyncio
 import ipaddress
 
-async def check_port(ip, port, loop):
-    conn = asyncio.open_connection(str(ip), port, loop=loop)
+
+async def check_port(ip, port):
+    conn = asyncio.open_connection(str(ip), port)
     try:
         reader, writer = await asyncio.wait_for(conn, timeout=3)
-        print(f'Port {port} on {ip} is open')
         writer.close()
         return True
     except:
         return False
 
-async def scan_ip(ip, ports, loop):
+
+async def scan_ip(ip, ports):
     open_ports = []
     for port in ports:
-        if await check_port(ip, port, loop):
+        if await check_port(ip, port):
             open_ports.append(port)
-    if open_ports:
-        print(f'{ip}: {open_ports}')
-    else:
-        print(f'{ip} has no open ports or is not responding.')
+    return (str(ip), open_ports)
+
 
 async def scan_network(network, ports):
     loop = asyncio.get_event_loop()
-    tasks = []
-    for ip in ipaddress.IPv4Network(network):
-        task = loop.create_task(scan_ip(ip, ports, loop))
-        tasks.append(task)
-    await asyncio.wait(tasks, timeout=5)
+    network = ipaddress.IPv4Network(network)
+    tasks = [loop.create_task(scan_ip(ip, ports)) for ip in network]
+    completed, pending = await asyncio.wait(tasks, timeout=5, return_when=asyncio.ALL_COMPLETED)
+
+    for task in completed:
+        ip, open_ports = task.result()
+        if open_ports:
+            print(f'{ip}: {open_ports}')
+        else:
+            print(f'{ip} has no open ports or is not responding.')
+
 
 if __name__ == '__main__':
     network_range = '192.168.1.0/24'  # Network range to scan
